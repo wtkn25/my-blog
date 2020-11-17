47952dc84be22f9fe0dab8810e07a13589a67f81
wtkn25 <atrastar72@gmail.com>
2020-11-17 18:08:00 +0900

    contentfulと連携してホームに記事一覧を作った

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,39 @@
 import React from "react"
+import { useStaticQuery, graphql } from 'gatsby';
 
 import Layout from '../components/layout';
 
 const Home = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulBlogPost(
+        sort: {fields: publishedDate, order: DESC },
+        limit: 5
+      ) {
+        edges {
+          node {
+            title
+            publishedDate
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <Layout>
-      ここがHOMEです
+      <h1>blog</h1>
+      <ul>
+        {data.allContentfulBlogPost.edges.map((edge) => {
+          return (
+            <li>
+              <h2>{edge.node.title}</h2>
+              <p>{edge.node.publishedDate}</p>
+            </li>
+          )
+        })}
+
+      </ul>
     </Layout>
   )
 }
