62bf75ad4af117fe510b70490238361c6c9e3d30
wtkn25 <atrastar72@gmail.com>
2020-11-18 22:36:32 +0900

    headerのスタイルの大枠作成

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
 
 const Header = () => {
   return (
-    <header>
-      <Link to="/">
-        <h1>iratawa.tech</h1>
-      </Link>
-    </header >
+    <Wrapper>
+      <HeaderContent>
+        <Link to="/">
+          <HPTitle>tech.iratawa</HPTitle>
+        </Link>
+      </HeaderContent>
+    </Wrapper >
   )
 }
 
 export default Header
+
+const Wrapper = styled.div`
+  width: 100%;
+`
+
+const HeaderContent = styled.div`
+  max-width: 980px;
+  margin: 0 auto;
+`
+
+const HPTitle = styled.h1`
+  color: #000000;
+  display: inline-block;
+  text-decoration: none;
+`
